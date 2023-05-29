4776e546915c0d9210624d51e09869f8e5c63092
hojooneum <bldolphin96@gmail.com>
2023-05-29 17:11:05 +0900

    feat(gpt): add gpt3.5 model

--- app.py
+++ app.py
@@ -1,43 +1,41 @@
 import os
 
 import openai
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, render_template, request
 from tenacity import retry, stop_after_attempt, wait_random_exponential
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-model = "text-davinci-003"
+models = {
+    "text-davinci": "text-davinci-003",
+    "gpt-3.5": "gpt-3.5-turbo"
+}
 conversation = []
-temperature = 0.3
+temperature = 0.5
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        response = get_openai_chat(request.form["user_dialog"])
-        conversation.append(response.choices[0].text)
-        return redirect(url_for("index", result=response.choices[0].text, conversation=conversation))
+        conversation.append(request.form["user_dialog"])
+        response = ""
+        try:
+            response = get_openai_chat(request.form["user_dialog"])
+        except:
+            response = "Chat bot is coming for you! Please try again later!"
+        conversation.append(response)
+        return render_template("index.html", conversation=conversation)
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+    return render_template("index.html", conversation=conversation)
 
 @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
 def get_openai_chat(user_dialog):
+    openai.Model.list()
     response = openai.Completion.create(
-        model=model,
+        model=models["text-davinci"],
         prompt=user_dialog,
         temperature=temperature
     )
-    return response
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+    print(response.choices)
+    return response.choices[0].text
