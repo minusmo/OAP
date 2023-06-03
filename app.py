392ba7ff5c110229cefb53feee9dacd0fcf972ff
hojooneum <bldolphin96@gmail.com>
2023-06-03 23:38:13 +0900

    feat(session): add session to store different conversation

--- app.py
+++ app.py
@@ -1,33 +1,36 @@
 import os
-
+import uuid
 import openai
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from tenacity import retry, stop_after_attempt, wait_random_exponential
 
 app = Flask(__name__)
+app.config.update(SECRET_KEY=str(uuid.uuid4()), SESSION_TYPE="chat_id")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 models = {
     "text-davinci": "text-davinci-003",
     "gpt-3.5": "gpt-3.5-turbo"
 }
-conversation = []
-temperature = 0.5
 
+temperature = 0.7
 
 @app.route("/", methods=("GET", "POST"))
 def index():
+    conversation = session.get('conversation', None)
+    if not conversation:
+        session['conversation'] = []
     if request.method == "POST":
-        conversation.append(request.form["user_dialog"])
-        response = ""
+        session['conversation'].append(request.form["user_dialog"])
+        openai_response = ""
         try:
-            response = get_openai_chat(request.form["user_dialog"])
+            openai_response = get_openai_chat(request.form["user_dialog"])
         except:
-            response = "Chat bot is coming for you! Please try again later!"
-        conversation.append(response)
-        return render_template("index.html", conversation=conversation)
+            openai_response = "Chat bot is coming for you! Please try again later!"
+        session['conversation'].append(openai_response)
+        return render_template("index.html", conversation=session['conversation'])
 
-    return render_template("index.html", conversation=conversation)
+    return render_template("index.html", conversation=session['conversation'])
 
 @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
 def get_openai_chat(user_dialog):
@@ -39,3 +42,6 @@ def get_openai_chat(user_dialog):
     )
     print(response.choices)
     return response.choices[0].text
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
